e86bb46a1b717ded7d4a7949b0fee2135ec62b9c
Hesham Mathana <heshammathana0@gmail.com>
2025-02-10 13:29:59 +0200

    Apply For Swaping By Overloading

---
+++ Session1/Apply For Generic/Apply For Generic/Helper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apply_For_Generic
+{
+    internal class Helper
+    {
+        public static void Swap(ref int x,ref int y)
+        {
+            int temp = x;
+            x = y;
+            y=temp;
+        }
+        public static void Swap(ref double x,ref double y)
+        {
+            double temp = x;
+            x = y;
+            y=temp;
+        }
+        public static void Swap(ref Point x,ref Point y)
+        {
+            Point temp = x;
+            x = y;
+            y=temp;
+        }
+    }
+}
---
+++ Session1/Apply For Generic/Apply For Generic/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Apply_For_Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 10;
+            //int y = 20;
+            //Helper.Swap(ref x,ref y);
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+
+            //double x = 10;
+            //double y = 20;
+            //Helper.Swap(ref x,ref y);
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            Point P1 = new Point(1,2);
+            Point P2 = new Point(4,3);
+            Helper.Swap(ref P2 ,ref P1);
+            Console.WriteLine(P1);
+            Console.WriteLine(P2);
+        }
+    }
+}
