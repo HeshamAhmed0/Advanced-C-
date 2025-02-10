28a6529e6833edc5d10ef7c5eb1512e8ee8308dc
Hesham Mathana <heshammathana0@gmail.com>
2025-02-10 14:38:10 +0200

    Apply For Generic Swap

--- Session1/Apply For Generic/Apply For Generic/Program.cs
+++ Session1/Apply For Generic/Apply For Generic/Program.cs
@@ -26,24 +26,24 @@
             #endregion
 
             #region Generic Swap
-            int x = 10;
-            int y = 20;
-            Helper.Swap(ref x, ref y);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-
-
-            //object x = 10;
-            //object y = 20;    // Boking
-            //Helper.Swap(ref x,ref y);
+            //int x = 10;
+            //int y = 20;
+            //Helper.Swap<int>(ref x, ref y);
             //Console.WriteLine(x);
             //Console.WriteLine(y);
 
-            //object P1 = new Point(1,2);
-            //object P2 = new Point(4,3);
-            //Helper.Swap(ref P2 ,ref P1);
+
+            //double x = 10;
+            //double y = 20;    
+            //Helper.Swap<double>(ref x, ref y);
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            //Point P1 = new Point(1, 2);
+            //Point P2 = new Point(4, 3);
+            //Helper.Swap<Point>(ref P2, ref P1);
             //Console.WriteLine(P1);
-            //Console.WriteLine(P2); 
+            //Console.WriteLine(P2);
             #endregion
         }
     }
