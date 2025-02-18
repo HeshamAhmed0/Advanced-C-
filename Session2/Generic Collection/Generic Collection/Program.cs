6f7ee98999a33534c15e2da2389f2192748e15eb
Hesham Mathana <heshammathana0@gmail.com>
2025-02-18 06:21:19 +0200

    Apply For Non Generic Collection ===> ArrayList

--- Session2/Generic Collection/Generic Collection/Program.cs
+++ Session2/Generic Collection/Generic Collection/Program.cs
@@ -7,11 +7,17 @@ namespace Generic_Collection
         static void Main(string[] args)
         {
             #region Non Generic Collections
+
+            //Add        AddRange 
+            // Will Add In The Last
+
+
+
             ArrayList arrayList = new ArrayList();
             // in This Time Allocate 0 Count And 0 Capacity
             Console.WriteLine($"({arrayList.Capacity} , {arrayList.Count})");  // 0 , 0
 
-            arrayList.Add( 1 );
+            arrayList.Add( 1 ); // Boking
             // Will Allocate 4 Capacity And Allocate 1 Count
             Console.WriteLine($"({arrayList.Capacity} , {arrayList.Count})"); // 4 ,1
 
@@ -22,6 +28,31 @@ namespace Generic_Collection
             arrayList.Add(10);
             // Will Duplicate Capacity For 8 And Count Will Increase 1
             Console.WriteLine($"({arrayList.Capacity} , {arrayList.Count})");  // 8 , 5
+
+            //foreach (int i in arrayList) { Console.WriteLine(i); }  // UnBoking
+
+            // TrimToSize =>>>>>>> This Func Will Remove lacer This is Empty From ArrayList 
+            // arrayList.TrimToSize();
+
+            #region Remove
+            //arrayList.Remove(1); // In This Case Will Remove The First Occeure For 1
+            //arrayList.RemoveAt(0); // In This Case Will Remove The Object In Index 0
+            //arrayList.RemoveRange(0, 2); //in this case will remove from index 0  ===>>>> 2 numbers
+            //Console.WriteLine($"({arrayList.Capacity} , {arrayList.Count})");
+            #endregion
+
+            #region Disadvantage ArrayList
+            arrayList.Add("Route");
+
+            // in this case will occure error because the data type 
+            // TO HANDLE THIS ERROR LOOK TO THE NEXT FOREACH    
+            //foreach (int i in arrayList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            foreach (var i in arrayList)
+            { Console.WriteLine(i); }
+            #endregion
 
             #endregion
         }
