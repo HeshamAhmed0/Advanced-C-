5ee7570c8325eae14273f87fd240dbb83334c45c
Hesham Mathana <heshammathana0@gmail.com>
2025-02-23 17:17:52 +0200

    Delegete

---
+++ Session3/Apply For Delegete/Apply For Delegete/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Apply_For_Delegete
+{
+    public delegate int CountFunc(string input);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CountFunc countFunc;
+            countFunc = new CountFunc(StringFunctions.CountUpper);
+            int Result = countFunc("HESHAM");
+            Console.WriteLine(Result);
+        }
+    }
+}
